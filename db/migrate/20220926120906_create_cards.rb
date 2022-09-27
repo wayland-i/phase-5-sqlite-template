413600cfc0d7858411256fffe9ed498fbbdf9eaa
Ian Iversen <ianiversen1@gmail.com>
2022-09-27 11:34:05 -0400

    public boolean added to card, now frontend behavior and backend database

--- db/migrate/20220926120906_create_cards.rb
+++ db/migrate/20220926120906_create_cards.rb
@@ -3,6 +3,7 @@ class CreateCards < ActiveRecord::Migration[7.0]
     create_table :cards do |t|
       t.string :title
       t.string :description
+      t.boolean :public
       t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
