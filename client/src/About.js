0cd220b809ccc5d8d9f510e7731ae2267e50c81f
Ian Iversen <ianiversen1@gmail.com>
2022-10-06 10:12:47 -0400

    this is presentable

--- client/src/About.js
+++ client/src/About.js
@@ -4,10 +4,15 @@ function About() {
   return (
     <div class="grid h-screen place-items-center m-1">
         {/* <h1 class="text-2xl font-bold mb-2">About</h1> */}
-        <h1 class="text-4xl font-bold mb-2">Hello and welcome to DEMO!</h1>
+        <h1 class="text-4xl font-bold mt-6">Hello and welcome to DEMO!</h1>
         <br></br>
-        <h1 class="text-2xl font-bold mb-2">This web application allows you to record audio tracks, listen back to them, and share them publically... all from your web browser!</h1>
+        <h1 class="text-2xl font-bold mb-2 mt-7">This web application allows you to record audio tracks, listen back to them, and share them publically... all from your web browser!</h1>
         <br></br>
+        <div class="flex">
+          <img src='/rockStar.jpg' width='400' height='400' alt='picture' class='m-40'></img>
+          {/* <h1>Meet the founder: Ian Iversen</h1> */}
+          <img src='/armsCrossed.jpg' width='400' height='400' alt='picture' class='m-40'></img>
+        </div>
         <div>
           <h1 class="text-4xl font-bold mb-10">Instructions</h1>
           <h1 class="text-2xl font-bold mb-2">Each User can make a DEMO card by navigating to their userpage and clicking the "new card" button.</h1>
